9e4756a621e2c270da2f4d4562cf222f88277284
vaxo <vaxojaniashvili186@gmail.com>
2024-05-09 15:50:34 +0400

    corrected

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,45 +1,6 @@
 "use client";
 import Link from "next/link";
-// import { useEffect, useState } from "react";
-
-// const HomePage = () => {
-//   const [products, setProducts] = useState([]);
-
-//   useEffect(() => {
-//     const fetchProducts = async () => {
-//       try {
-//         const response = await fetch("https://fakestoreapi.com/products");
-//         if (!response.ok) {
-//           throw new Error("Failed to fetch products");
-//         }
-//         const data = await response.json();
-//         setProducts(data);
-//       } catch (error) {
-//         console.error("Error fetching products:", error);
-//       }
-//     };
-
-//     fetchProducts();
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Products</h1>
-//       <ul>
-//         {products.map((product: any) => (
-//           <div>
-//             <li key={product.id}>
-//               <h2>{product.title}</h2>
-//             </li>
-//           </div>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default HomePage;
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
   const [data, setData] = useState([]);
