4106d72e19c4ec2382bdb806cd5db9d717a59a59
vaxo <vaxojaniashvili186@gmail.com>
2024-05-14 23:29:33 +0400

    fetch and useParams

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,70 +1,44 @@
 "use client";
-import { Fetch } from "@/app/types/common";
+import Image from "next/image";
 import Link from "next/link";
-// import Link from "next/link";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-// const HomePage = () => {
-//   const [data, setData] = useState([]);
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const res = await fetch("https://fakestoreapi.com/products");
-//         const result = await res.json();
-//         setData(result);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-//     fetchData();
-//   }, []);
-//   return (
-//     <div>
-//       <h1>products</h1>
-//       <div>
-//         {data.map((item: any) => {
-//           return (
-//             <div>
-//               <Link href={`/pages/home/${item.id}`}>
-//                 <div className="flex font-serif py-2">{item.title}</div>
-//               </Link>
-//             </div>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default HomePage;
-import React, { useEffect, useState } from "react";
-
-const page = () => {
+const HomePage = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await fetch("https://fakestoreapi.com/products");
-        setData(await res.json());
+        const result = await res.json();
+        setData(result);
       } catch (error) {
-        console.log(error, "error");
+        console.log(error);
       }
     };
     fetchData();
   }, []);
   return (
     <div>
-      {data.map((item: Fetch) => {
-        return (
-          <div key={item.id}>
-            <Link href={`/pages/home/${item.id}`}>
-              <h1>{item.title}</h1>
-            </Link>
-          </div>
-        );
-      })}
+      <div className="flex justify-center">
+        <Link href="/pages/Basket">
+          <p className="w-fit px-5 text-white bg-black rounded">Basket</p>
+        </Link>
+      </div>
+      <h1 className="px-5">products</h1>
+      <div>
+        {data.map((item: any) => {
+          return (
+            <div className="px-5">
+              <Link href={`/pages/home/${item.id}`}>
+                <div className="flex font-serif py-2">{item.title}</div>
+                <Image width={150} height={150} src={item.image} />
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
 
-export default page;
+export default HomePage;
--- src/app/types/common.ts
+++ src/app/types/common.ts
@@ -1,9 +1,16 @@
 export interface Fetch {
-  id: number;
+  [x: string]: string | undefined;
+  id: any;
   name: string;
-  title: React.ReactNode;
-
+  title: any;
   description: string;
-
+  price: any;
+  image: any;
+  src: any
+}
+export interface Product {
+  id: number;
+  title: string;
+  image: string;
   price: number;
 }
